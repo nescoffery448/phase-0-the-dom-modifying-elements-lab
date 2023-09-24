b06183e33b40ee64fcd778ba6cf3c40ef1c3b579
Nyanibah Escoffery <naomi@diamondrecovery.net>
2023-09-24 17:05:11 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove()
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.textContent = "Naomi is the champion";
+document.body.append(newHeader)
